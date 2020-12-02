2e151103e991b6794223e009bd9ec2a67764eec8
Jackie <taekwondo.chan@gmail.com>
2020-12-01 18:00:15 -0800

    created savory class

--- main.py
+++ main.py
@@ -42,9 +42,40 @@ class Welcome:
         print(f"User's name changed to: {self.username}")
 
 
+# Inherits username, displays recipe options
+class Savory(Welcome):
+    def __init__(self, username, hunger = True, chosen_item = ""):
+        super().__init__(username, hunger)
+        self.chosen_item = ""
+
+    def known_info(self):
+        """Prints user's information"""
+
+        print(f"Username: {self.username}")
+        print(f"{self.username} wants savory")
+
+    def display_recipes(self):
+        """Display savory recipes"""
+
+        while True:
+            recipe_name = input("What tickles your fancy today? Some good ol' meat between buns or an Italian pie? ")
+
+            if recipe_name.lower() == "burger" or recipe_name.lower() == "meat between buns" or recipe_name.lower() == "meat":
+                print("I see you want a burger.")
+                self.chosen_item = "burger"
+                break
+            elif recipe_name.lower() == "pizza" or recipe_name.lower() == "italian pie" or recipe_name.lower() == "pie":
+                print("I see you want a pizza.")
+                self.chosen_item = "pizza"
+                break
+            else:
+                print("I have no idea what that is my dude. Try again.")
+                continue
+
+        return self.chosen_item
+
+
 #------------------- Object Creation -------------------
-test_user = Welcome("testuser", True)
+test_user = Savory("testuser")
 test_user.known_info()
-test_user.is_hungry()
-test_user.change_name()
-test_user.known_info()
+test_user.display_recipes()
